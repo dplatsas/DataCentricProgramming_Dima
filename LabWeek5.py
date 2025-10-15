9a448ea4c36c9fa48d95b2272a47d32bd1825f78
dplatsas <114009023+dplatsas@users.noreply.github.com>
2025-10-15 13:55:21 +0100

    Added load_abc_file function

--- LabWeek5.py
+++ LabWeek5.py
@@ -1,1 +1,6 @@
-
+# Task 2.1: Load the File
+def load_abc_file(filename):
+    """Load ABC file into list of lines"""
+    with open(filename, 'r', encoding='latin-1') as f:
+        lines = f.readlines()
+    return lines
